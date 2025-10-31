9719d2761a6e9f8dff9cf2fabe477482f1cd91c4
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-10-31 23:22:45 +0200

    feat: configure global settings with ConfigModule,  enable CORS with cookie support

--- server-side/src/main.ts
+++ server-side/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(cookieParser());
+  app.enableCors({
+    origin: process.env.CLIENT_URL ?? 'http://localhost:5173',
+    credentials: true,
+    exposedHeaders: ['set-cookie'],
+  });
+
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
