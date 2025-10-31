a7bcf1640c6db4eba0676b0d4d58cee0c04bbffb
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-10-31 23:22:17 +0200

    fix: update ESLint configuration and correct user model field name

--- server-side/eslint.config.mjs
+++ server-side/eslint.config.mjs
@@ -19,7 +19,7 @@ export default tseslint.config(
       },
       sourceType: 'commonjs',
       parserOptions: {
-        projectService: true,
+        project: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
