c43ffc2eef520907947521b26179c05ec73e0090
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-10-31 23:21:55 +0200

    feat: implement authentication module with login and registration functionality

---
+++ server-side/src/auth/auth.service.ts
@@ -0,0 +1,50 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma.service';
+import { UserService } from 'src/user/user.service';
+import { AuthDto } from './dto/auth.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private jwt: JwtService,
+    private userService: UserService,
+    private prisma: PrismaService,
+  ) {}
+
+  async login(dto: AuthDto) {
+    const user = await this.validateUser(dto);
+    const tokens = this.issueTokens(user.id);
+    return { user, ...tokens };
+  }
+
+  async register(dto: AuthDto) {
+    const existingUser = await this.userService.getByEmail(dto.email);
+    if (existingUser) {
+      throw new NotFoundException('User already exists');
+    }
+    const user = await this.userService.create(dto);
+    const tokens = this.issueTokens(user.id);
+    return { user, ...tokens };
+  }
+
+  issueTokens(userId: string) {
+    const data = { id: userId };
+    const accessToken = this.jwt.sign(data, {
+      expiresIn: '1h',
+    });
+
+    const refreshToken = this.jwt.sign(data, {
+      expiresIn: '7d',
+    });
+    return { accessToken, refreshToken };
+  }
+
+  private async validateUser(dto: AuthDto) {
+    const user = await this.userService.getByEmail(dto.email);
+
+    if (!user) throw new NotFoundException('User not found');
+
+    return user;
+  }
+}
