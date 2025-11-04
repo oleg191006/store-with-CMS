10a20e25f0c1da9c04f100e0fa7b35f8e4a37b5e
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-11-04 16:04:21 +0200

    feat: add additional modules to the application for enhanced functionality

--- server-side/src/app.module.ts
+++ server-side/src/app.module.ts
@@ -2,8 +2,26 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { OrderModule } from './order/order.module';
+import { ColorModule } from './color/color.module';
+import { CategoryModule } from './category/category.module';
+import { FileModule } from './file/file.module';
+import { StoreModule } from './store/store.module';
+import { StatisticsModule } from './statistics/statistics.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    OrderModule,
+    ColorModule,
+    CategoryModule,
+    FileModule,
+    StoreModule,
+    StatisticsModule,
+    ProductModule,
+  ],
 })
 export class AppModule {}
