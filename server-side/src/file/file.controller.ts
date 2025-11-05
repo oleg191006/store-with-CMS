ba996e1b828bea22236115a00d56faca8ad39da3
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-11-05 22:28:55 +0200

    feat: implement file upload functionality with directory management and response structure

--- server-side/src/file/file.controller.ts
+++ server-side/src/file/file.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  HttpCode,
+  Post,
+  Query,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileService } from './file.service';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { FilesInterceptor } from '@nestjs/platform-express';
 
-@Controller('file')
+@Controller('files')
 export class FileController {
   constructor(private readonly fileService: FileService) {}
+
+  @HttpCode(200)
+  @UseInterceptors(FilesInterceptor('files'))
+  @Auth()
+  @Post()
+  async saveFiles(
+    @UploadedFiles() files: Express.Multer.File[],
+    @Query('folder') folder?: string,
+  ) {
+    return this.fileService.saveFiles([...files], folder);
+  }
 }
