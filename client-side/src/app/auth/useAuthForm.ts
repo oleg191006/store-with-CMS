bc65f8e167aafc3088e4dd0c72cead4dc795cc51
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-11-12 19:08:36 +0200

    feat: implement custom hook for authentication form handling

---
+++ client-side/src/app/auth/useAuthForm.ts
@@ -0,0 +1,38 @@
+import { PUBLIC_URL } from "@/config/url.config";
+import { authService } from "@/services/auth/auth.service";
+import { IAuthForm } from "@/shared/types/auth.interface";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+import { SubmitHandler, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+
+export function useAuthForm(isReg: boolean) {
+  const router = useRouter();
+  const form = useForm<IAuthForm>({
+    mode: "onChange",
+  });
+
+  const { mutate, isPending } = useMutation({
+    mutationKey: ["auth user"],
+    mutationFn: (data: IAuthForm) =>
+      authService.main(isReg ? "register" : "login", data),
+    onSuccess() {
+      form.reset();
+      toast.success("Successfully authenticated!");
+      router.replace(PUBLIC_URL.home());
+    },
+    onError(error) {
+      if (error.message) {
+        toast.error(error.message);
+      } else {
+        toast.error("Authentication failed. Please try again.");
+      }
+    },
+  });
+
+  const onSubmit: SubmitHandler<IAuthForm> = (data) => {
+    mutate(data);
+  };
+
+  return { onSubmit, form, isPending };
+}
