7ec2d0259e69ec60e57d601c5b57b53e17d2eb08
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-11-15 22:12:22 +0200

    feat: add Tailwind CSS configuration and update PostCSS setup

--- client-side/next.config.ts
+++ client-side/next.config.ts
@@ -15,6 +15,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  sassOptions: {
+    includePaths: ["./src"],
+  },
   async rewrites() {
     return [
       {
---
+++ client-side/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "hsl(var(--primary))",
+                "primary-foreground": "hsl(var(--primary-foreground))",
+                secondary: "hsl(var(--secondary))",
+                "secondary-foreground": "hsl(var(--secondary-foreground))",
+                destructive: "hsl(var(--destructive))",
+                "destructive-foreground": "hsl(var(--destructive-foreground))",
+                // і так далі…
+            },
+        },
+    },
+    plugins: [],
+}
