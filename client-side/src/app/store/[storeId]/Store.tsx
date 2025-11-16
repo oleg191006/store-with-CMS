07830bd2eb7c88915a86c8cf2284ae428a85c9bb
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-11-16 18:47:22 +0200

    feat: integrate MiddleStatistics component into Store

--- client-side/src/app/store/[storeId]/Store.tsx
+++ client-side/src/app/store/[storeId]/Store.tsx
@@ -3,12 +3,14 @@
 import { Heading } from "@/components/ui/heading";
 import styles from "./Store.module.scss";
 import { MainStatistics } from "./statistics/main-statitics/MainStatistics";
+import { MiddleStatistics } from "./statistics/middle-statistics/MiddleStatistics";
 
 export function Store() {
   return (
     <div className={styles.wrapper}>
       <Heading title="Statistics" />
       <MainStatistics />
+      <MiddleStatistics />
     </div>
   );
 }
