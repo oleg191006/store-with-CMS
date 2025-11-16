886a1946360455997212bf9ebdc4d0c016129ac6
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-11-16 14:12:00 +0200

    feat: add Header component with user profile integration and mobile sidebar

--- client-side/src/components/layouts/store-layout/sidebar/MobileSidebar.tsx
+++ client-side/src/components/layouts/store-layout/sidebar/MobileSidebar.tsx
@@ -5,7 +5,7 @@ import { Sidebar } from "./Sidebar";
 export function MobileSidebar() {
   return (
     <Sheet>
-      <SheetTrigger className="hidden pr-4 transition lg: hover:opacity-75">
+      <SheetTrigger className="pr-4 transition hover:opacity-75 lg:hidden">
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-white">
