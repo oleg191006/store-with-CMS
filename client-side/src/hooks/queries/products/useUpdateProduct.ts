527c5e41c6bc586ce3c68afc0d8cac5c04cf1f83
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-11-22 01:39:45 +0200

    feat: update product hook to include product ID validation and redirect after successful update

--- client-side/src/hooks/queries/products/useUpdateProduct.ts
+++ client-side/src/hooks/queries/products/useUpdateProduct.ts
@@ -3,35 +3,42 @@
 import { productService } from "@/services/product.service";
 import { IProductInput } from "@/shared/types/product.interface";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useParams } from "next/navigation";
-import { useMemo } from "react";
+import { useParams, useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 
 export const useUpdateProduct = () => {
-  const params = useParams<{ storeId: string }>();
-
+  const params = useParams<{ storeId: string; productId: string }>();
+  const router = useRouter();
   const queryClient = useQueryClient();
 
   const { mutate: updateProduct, isPending: isLoadingUpdate } = useMutation({
     mutationKey: ["update product"],
-    mutationFn: (data: IProductInput) =>
-      productService.update(params.storeId, data),
+    mutationFn: (data: IProductInput) => {
+      if (!params?.productId) {
+        throw new Error("Product ID is missing");
+      }
+      return productService.update(params.productId, data);
+    },
     onSuccess() {
       queryClient.invalidateQueries({
         queryKey: ["get products for store dashboard"],
       });
+
       toast.success("Product updated successfully");
+
+      if (params?.storeId) {
+        setTimeout(() => {
+          router.push(`/store/${params.storeId}/products`);
+        }, 1500);
+      }
     },
     onError() {
       toast.error("Failed to update product. Please try again.");
     },
   });
 
-  return useMemo(
-    () => ({
-      updateProduct,
-      isLoadingUpdate,
-    }),
-    [updateProduct, isLoadingUpdate]
-  );
+  return {
+    updateProduct,
+    isLoadingUpdate,
+  };
 };
