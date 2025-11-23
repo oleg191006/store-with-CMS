e47b060501e0b976adac449dcceb307c67e2f9fe
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-11-23 16:55:07 +0200

    feat: add Header component with styling and search input integration

---
+++ client-side/src/components/layouts/main-layout/header/Header.tsx
@@ -0,0 +1,16 @@
+import { HeaderMenu } from "./header-menu/HeaderMenu";
+import styles from "./Header.module.scss";
+import { Logo } from "./logo/Logo";
+import { SearchInput } from "./search-input/SearchInput";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <Logo />
+      <div className={styles.search}>
+        <SearchInput />
+      </div>
+      <HeaderMenu />
+    </header>
+  );
+}
