f81a949540b6ac51befe34abcb408302c05fd1fe
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-11-23 20:04:05 +0200

    feat: add Catalog component with styling and product listing functionality; update site description for SEO

--- client-side/src/constants/seo.constants.ts
+++ client-side/src/constants/seo.constants.ts
@@ -8,4 +8,4 @@ export const NO_INDEX_PAGE = {
 export const SITE_NAME = "Vendens";
 
 export const SITE_DESCRIPTION =
-  "An example e-commerce site built with Next.js and TypeScript.";
+  "Vendens is a cutting-edge e-commerce platform designed to empower businesses of all sizes to create, manage, and grow their online stores with ease. Our platform offers a comprehensive suite of tools and features that streamline the entire e-commerce process, from product listing and inventory management to payment processing and customer engagement. With Vendens, businesses can customize their storefronts, optimize their operations, and deliver exceptional shopping experiences to their customers. Whether you're a small boutique or a large enterprise, Vendens provides the flexibility and scalability needed to succeed in the competitive world of online retail.";
