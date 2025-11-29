7a2ddc45e812142bba7544c70b1ff9f96e3a15db
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-11-29 22:08:53 +0200

    feat: enhance Home component to accept products as props and display popular items in Catalog

--- client-side/src/app/(root)/Home.tsx
+++ client-side/src/app/(root)/Home.tsx
@@ -1,9 +1,23 @@
+import { IProduct } from "@/shared/types/product.interface";
 import { Hero } from "./hero/Hero";
+import { Catalog } from "@/components/ui/catalog/Catalog";
+import { PUBLIC_URL } from "@/config/url.config";
 
-export function Home() {
+interface HomeProps {
+  products: IProduct[];
+}
+
+export function Home({ products }: HomeProps) {
   return (
     <>
       <Hero />
+      <Catalog
+        title="Hits sale"
+        description="Most popular products in our store"
+        linkTitle="See more"
+        link={PUBLIC_URL.explorer()}
+        products={products}
+      />
     </>
   );
 }
